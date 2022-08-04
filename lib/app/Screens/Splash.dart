247f1833acc6f63026acf46d2895b6986d195d20
HarshDixit <harshdixit991@gmail.com>
2022-08-04 16:47:50 +0530

    Teacher App Done for darwar shab

--- lib/app/Screens/Splash.dart
+++ lib/app/Screens/Splash.dart
@@ -1,9 +1,12 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:qr_scanner_app/app/Screens/commom_functions.dart';
 import 'package:qr_scanner_app/app/Screens/login.dart';
 import 'package:qr_scanner_app/app/Screens/qr_scaner.dart';
 import 'package:qr_scanner_app/app/utilities/UserSecureStorage.dart';
+
+import '../bloc/login_bloc.dart';
 
 class Splash extends StatefulWidget {
   const Splash({Key? key}) : super(key: key);
@@ -14,9 +17,14 @@ class Splash extends StatefulWidget {
 
 class _SplashState extends State<Splash> {
   String? finalToken;
+  late String email, password;
+  late LoginBloc _loginBloc;
+  late CommonFuntion _commonFuntion;
 
   @override
   void initState() {
+    _commonFuntion = CommonFuntion();
+    _loginBloc = LoginBloc();
     setLocal();
     super.initState();
   }
@@ -34,7 +42,52 @@ class _SplashState extends State<Splash> {
        _buildChild();
 
   }
+  autoLogin() async {
+    // _commonFuntion.loaderHandlerShow(context);
+    var loginData = {
+      "email": "deepsingh@ssism.org",
+      "password": "ssism@deepsingh"
+    };
+    // var loginData = {"email": email, "password": password};
+    final loginResponse = await _loginBloc.fetchLogin(loginData).then((value) {
+      final DateTime now = DateTime.now();
+      final DateFormat formatter = DateFormat('h');
+      final String formatted = formatter.format(now);
+      print(formatted);
+      final dateFormatter = DateFormat.yMd();
+      final String formatteddat = dateFormatter.format(now);
+      print(formatteddat);
 
+      var data = int.parse(formatted.toString());
+
+      print((data + 3).toString());
+
+      UserSecureStorage.setLoginTime((data + 3).toString());
+      UserSecureStorage.setLoginDate(formatteddat.toString());
+
+      // something like 2013-04-20
+
+      // _commonFuntion.loaderHandlerHide();
+
+      Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(
+              builder: (BuildContext context) => const QRScaner()),
+          (Route<dynamic> route) => false);
+    }).catchError((err) {
+      // _commonFuntion.loaderHandlerHide();
+      print("error is that " + err.toString());
+
+      _commonFuntion.snackBar(
+          err == 404
+              ? "User Not Found"
+              : err == 400
+                  ? "Invalid Cradential"
+                  : err.toString(),
+          context);
+    });
+    print("Login Response  " + loginResponse.toString());
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -84,10 +137,11 @@ class _SplashState extends State<Splash> {
           (route) => false);
       }
      else{
- Navigator.pushAndRemoveUntil(
-          context,
-          MaterialPageRoute(builder: (context) => const LoginScreen()),
-          (route) => false);
+      autoLogin();
+//  Navigator.pushAndRemoveUntil(
+//           context,
+//           MaterialPageRoute(builder: (context) => const LoginScreen()),
+//           (route) => false);
       }
     });
   }
