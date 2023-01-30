799809d643c197e95b762c0b117669aa9fc360db
HarshDixit54 <harsh.dixit@baelworks.com>
2023-01-30 10:30:20 +0530

    base url change of darwar sahab apk

--- lib/app/utilities/Constants.dart
+++ lib/app/utilities/Constants.dart
@@ -1,7 +1,8 @@
 class Constants {
 
   // static const String BASEURL ="https://api.ssism.org/api/";
-  static const String BASEURL ="https://backend.ssism.org/api/";
+  static const String BASEURL ="https://cdp.baelworks.com/api/";
+  // static const String BASEURL ="https://backend.ssism.org/api/";
   // static const String BASEURL ="https://singaji-central-server-master.herokuapp.com/api/";
   // static const String BASEURL ="https://singaji-central-server-master.herokuapp.com/api/add_employee_attendance?userId=";
   
